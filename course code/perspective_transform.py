3ef4e27d6698eb0f66a414ea1b7e9645a9804d30
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-06-03 17:11:00 -0500

    draw the images

--- course code/perspective_transform.py
+++ course code/perspective_transform.py
@@ -41,3 +41,20 @@ destination = np.float32(des_left_bottom, des_left_top,
                          des_right_top, des_right_bottom)
 # get warped image
 warped_image = perspect_transform(image, source, destination)
+
+# draw source and destination points on images
+# draw source in blue
+cv2.polylines(image, np.int32([source]), True, (0, 0, 255), 3)
+# draw destination in green
+cv2.polylines(warped_image, np.int32([destination]), True, (0, 255, 0), 3)
+
+# display figure
+f, (ax1, ax2) = plt.subplots(1, 2, figsize = (24, 6), sharey= True)
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize = 40)
+
+ax2.imshow(warped_image, cmap = 'gray')
+ax2.set_title('Warped Image', fontsize = 40)
+plt.subplots_adjust(left = 0, right = 1, top = 0.9, bottom = 0)
+plt.show()
