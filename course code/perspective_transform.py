a455021c62e4fb119d04d8bcdd2035f08b92a1b0
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-06-04 22:41:53 -0500

    move the source and destination into helper

--- course code/perspective_transform.py
+++ course code/perspective_transform.py
@@ -1,33 +1,10 @@
 import cv2
-import matplotlib.image as mpimg
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-from helper_functions import perspect_transform
+from helper_functions import perspect_transform, source, destination, image
 
-# read the grid image
-image = mpimg.imread('example_grid1.jpg')
-
-# define source point
-src_left_bottom = [15.629, 141.177]
-src_left_top = [118.855, 96.0161]
-src_right_top = [199.5, 96.6613]
-src_right_bottom = [301.435, 141.177]
-
-source = np.float32([src_left_bottom, src_left_top,
-                    src_right_top, src_right_bottom])
-
-# define destination point
-image_center_x = image.shape[0] / 2
-image_near_bottom_y = image.shape[1] - 10
-
-des_left_bottom = [image_center_x - 5, image_near_bottom_y]
-des_left_top = [des_left_bottom[0], des_left_bottom[1] - 10]
-des_right_top = [des_left_top[0] + 10, des_left_top[1]]
-des_right_bottom = [des_right_top[0], des_right_top[1] + 10]
-
-destination = np.float32([des_left_bottom, des_left_top,
-                         des_right_top, des_right_bottom])
 # get warped image
 warped_image = perspect_transform(image, source, destination)
 
